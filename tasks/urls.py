c7318c8f63cb56b0a2e7c1692234d319a7b2850d
ad1k4 <241337@astanait.edu.kz>
2026-02-06 17:25:55 +0500

    Added registration feature by Adilet

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    
     path('', views.task_list, name='task_list'),
+
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('register/', views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
